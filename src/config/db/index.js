4f34325ebe7d9e6cb1f350ffa53a160011b0b170
TP-O <letranphong2k1@gmail.com>
2022-12-15 18:08:14 +0700

    c11

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -2,9 +2,16 @@ const mongoose = require('mongoose');
 
 async function connect () {
     try {
-        await mongoose.connect('mongodb://mongo:27017/F8_education_dev');
-        console.log('Connect successfully!!!');
+        await mongoose.connect('mongodb://mongo:27017',{
+            user: 'user2022',
+            pass: 'user_password',
+            dbName: 'F8_education_dev'
+        });
+        for (let i =0; i < 10; i++) {
+            console.log('Connect successfully!!!');
+        }
     } catch (error) {
+        console.log('mongodb://user2022:user_password@mongo:27017/F8_education_dev')
         console.log('Connect failed!!!');
         throw error
     }
